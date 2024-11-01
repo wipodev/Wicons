60586c077151b8a7cc7049de49c9b0a7a0e0817f
wipodev <ajwipo@gmail.com>
2024-10-31 20:10:56 -0500

    remove the option to name the selected icons I prefer the selected icons to be all those that are in the folder provided by the user

--- bin/Cli.js
+++ bin/Cli.js
@@ -10,7 +10,6 @@ import { getRoutesSVG, ensureDirectoryExists } from "../lib/utils.js";
 program
   .option("-p, --path <path>", "Path to SVG icons folder", join(process.cwd(), "lib/svg"))
   .option("-m, --mode <mode>", "Execution mode (build or dev)", "build")
-  .option("-i, --icons <icons>", "Comma-separated list of icons to use", "")
   .option("-o, --output <output>", "Output folder", null)
   .option("-f, --filename <filename>", "Output file name", null)
   .option("-e, --embed", "Embed SVGs as Data URIs", false);
@@ -21,8 +20,7 @@ const options = program.opts();
 const svgPath = options.path;
 const mode = options.mode;
 const embed = options.embed;
-const selectedIcons = options.icons ? options.icons.split(",") : null;
-const outputDir = options.output || (mode === "dev" ? "src" : "dist");
+const outputDir = options.output || (mode === "dev" ? "dev" : "dist");
 
 const embedPart = embed ? "embed" : "routes";
 const modePart = mode === "build" ? ".min" : "";
@@ -31,7 +29,7 @@ const outputFilename = options.filename || `wicons.${embedPart}${modePart}.css`;
 const outputPath = ensureDirectoryExists(outputDir);
 
 const routesSVG = getRoutesSVG(svgPath, embed);
-const iconsToUse = selectedIcons || Object.keys(routesSVG);
+const iconsToUse = Object.keys(routesSVG);
 const cssContent = generateWicons(iconsToUse, routesSVG);
 
 const outputCss = mode === "build" ? minify(cssContent).css : cssContent;
