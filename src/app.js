eb92ba36457ef5665fd420f8d4109c2ac0f1012b
wipodev <ajwipo@gmail.com>
2024-11-23 19:55:18 -0500

    Removed unnecessary files and updated the wivex library

--- src/app.js
+++
@@ -1,9 +0,0 @@
-import { generateCSS } from "./assets/js/main.js";
-import { detectLoadSVG, loadTheme, handleMenu } from "./assets/js/utils.js";
-
-window.addEventListener("load", () => {
-  loadTheme();
-  handleMenu();
-  generateCSS();
-  detectLoadSVG();
-});
